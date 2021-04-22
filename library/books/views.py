26ec4306b5c4da5e8ce41c7102d382d9d3f37a5f
Vsevolod Mycroft <mycroft@deeplay.io>
2021-04-22 22:37:01 +1000

    Add `author` query param for `BookViewSet.get_queryset` of `books` app
    This allows to filter books by authors by providing author names in
    the `author` query param.

--- library/books/views.py
+++ library/books/views.py
@@ -14,3 +14,11 @@ class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = Book.objects.all()
+        authors = self.request.query_params.get("authors")
+        if authors is not None:
+            authors = authors.split(",")
+            queryset = queryset.filter(authors__name__in=authors)
+        return queryset
