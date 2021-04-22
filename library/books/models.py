987468b84bf165a9ea0bdc9a364b4e62ad938f41
Vsevolod Mycroft <mycroft@deeplay.io>
2021-04-22 21:38:11 +1000

    Add `Book` model and extend `Author` model in `books` app

--- library/books/models.py
+++ library/books/models.py
@@ -4,5 +4,24 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(verbose_name="Name", max_length=50)
 
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "Author"
+        verbose_name_plural = "Authors"
+
     def __str__(self):
         return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(verbose_name="Title", max_length=250)
+    description = models.TextField(verbose_name="Description")
+    authors = models.ManyToManyField(Author)
+
+    class Meta:
+        ordering = ["title"]
+        verbose_name = "Book"
+        verbose_name_plural = "Books"
+
+    def __str__(self):
+        return self.title
