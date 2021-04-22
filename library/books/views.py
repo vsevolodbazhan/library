d52d680328e3fafce22eb294375fa789c1a463a7
Vsevolod Mycroft <mycroft@deeplay.io>
2021-04-22 22:04:21 +1000

    Add viewsets and urls for `Author` and `Book` models of `books` app

--- library/books/views.py
+++ library/books/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
 
-# Create your views here.
+from library.books.models import Author, Book
+from library.books.serializers import AuthorSerializer, BookSerializer
+
+
+class AuthorViewSet(viewsets.ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
