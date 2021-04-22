d9f928589bd7448748540d090185e9085cc1a1b9
Vsevolod Mycroft <mycroft@deeplay.io>
2021-04-22 21:35:16 +1000

    Add `Author` model to `books` app

--- library/books/models.py
+++ library/books/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(verbose_name="Name", max_length=50)
+
+    def __str__(self):
+        return self.name
